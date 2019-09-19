0b693f5405a50696d48cd06b154f8b67aca52487
Luke Swanson <swansonl@cs.wisc.edu>
2019-09-19 14:54:23 -0500

    Add in the web page for the classroom portal, and its associated files

---
+++ classroom_portal.js
@@ -0,0 +1,34 @@
+function onload() {
+    game_options = {
+        "lakeland": {
+            "display_name": "Lakeland",
+            "bg_image": "https://fielddaylab.wisc.edu/assets/img/hero-images/lakeland.jpg",
+            "redirect": "https://fielddaylab.wisc.edu/play/lakeland/usda/iframe.html"
+        },
+        "jowilder": {
+            "display_name": "Jo Wilder and the Captiol Case",
+            "bg_image": "https://fielddaylab.wisc.edu/assets/img/hero-images/jowilder-bg.jpg",
+            "redirect": "https://fielddaylab.wisc.edu/play/jowilder/game/iframe.html"
+        }
+    }
+    let url = window.location.href.toString();
+    let paramstring = url.split("?");
+    paramstring = paramstring[1].split("&");
+    let params = {};
+    paramstring.forEach(function(item, index) {let param = item.split("="); params[param[0]] = param[1];} );
+
+    var selected_game_option = game_options[params["game"]];
+
+    let elem_game_name = document.getElementById("game_name");
+    elem_game_name.innerHTML = elem_game_name.innerHTML.replace("$GAMENAME", selected_game_option["display_name"]);
+    document.body.style.backgroundImage = `url(${selected_game_option["bg_image"]})`;
+    let id_form = document.getElementById("id_form");
+    id_form.action = selected_game_option["redirect"];
+    let id_box = document.getElementById("id_box");
+    id_box.oninvalid = function(event) {
+        id_box.setCustomValidity("");
+        if (!event.target.validity.valid) {
+            id_box.setCustomValidity("Your Player ID should be a letter followed by three numbers.");
+        }
+    }
+}
